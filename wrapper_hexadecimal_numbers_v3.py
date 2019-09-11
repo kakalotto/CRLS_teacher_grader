051f42cf1e7370d7f9d7a601a2cb8aab0787b2c3
Eric Wu <dimmyfinster@samanthsmacbook.lan>
2019-09-10 21:42:26 -0400

    update untested

--- wrapper_hexadecimal_numbers_v3.py
+++ wrapper_hexadecimal_numbers_v3.py
@@ -1,6 +1,6 @@
 # Pass these in as parameters
 from master_grader import master_grader
-from CRLS_APCSP_autograder.app.hex_minilab import docs_feedback_hex_minilab
+from CRLS_APCSP_autograder.app.hexadecimal_numbers_v3 import docs_feedback_hexadecimal_numbers_v3
 
 fulltext_search = 'hexadecimal_numbers_v3'
 
@@ -16,4 +16,4 @@ value_cells = ['B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'B11','B12','B13
 rubric_sheet_name = 'Sheet1'
 
 master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
-              scorer=docs_feedback_hex_minilab)
+              scorer=docs_feedback_hexadecimal_numbers_v3)
