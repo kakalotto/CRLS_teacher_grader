8f5dcb1b2df77143c74b4fde3c5730282888d3e7
Eric Wu <ewu@cpsd.us>
2019-09-25 13:55:24 -0400

    added 2.051

---
+++ wrapper_python_2.051.py
@@ -0,0 +1,46 @@
+# Pass these in as parameters
+import sys
+from master_grader import master_grader
+from CRLS_APCSP_autograder.app.python_2051 import feedback_2051
+
+person = ''
+if len(sys.argv) > 1:
+    person = sys.argv[1]
+
+
+def doc_name_to_rubric_name(doc_name):
+    import re
+    p_rubric_name = doc_name
+    p_rubric_name = re.sub(r'lab',
+                           r'rubric',
+                           p_rubric_name)
+    return p_rubric_name
+
+
+
+fulltext_search = '.py'
+value_cells = ['F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'B9', 'B4', ]
+rubric_sheet_name = ''
+
+if not person:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=feedback_2040, python_lab_num='2.051',
+                  python_rubric_suffix=' -  Python_2.051_Game_show_2_CRLS_voter_rubric')
+else:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=feedback_2040, python_lab_num='2.051',
+                  python_rubric_suffix='  Python_2.051_Game_show_2_CRLS_voter_rubric', person=person)
+
+fulltext_search = 'Game_Show'
+
+value_cells = ['F4', ]
+rubric_sheet_name = ''
+
+if not person:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=docs_feedback_python_2051)
+else:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=docs_feedback_python_2051, person=person)
+
+print("oooo" )
