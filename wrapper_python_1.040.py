10ff283496a576a7514ae371c58aaa4e4e5c606c
eric Wu <ewu@cpsd.us>
2019-09-30 00:34:23 +0000

    updated memory wrapper

--- wrapper_python_1.040.py
+++ wrapper_python_1.040.py
@@ -1,8 +1,12 @@
 # Pass these in as parameters
 from master_grader import master_grader
 from CRLS_APCSP_autograder.app.python_1040 import feedback_1040
+import sys
 
 fulltext_search = '.py'
+person = ''
+if len(sys.argv) > 1:
+    person = sys.argv[1]
 
 
 def doc_name_to_rubric_name(doc_name):
@@ -17,6 +21,10 @@ def doc_name_to_rubric_name(doc_name):
 value_cells = ['F4', 'F5', 'F6', 'F7', 'F8', 'B9', 'B4', ]
 rubric_sheet_name = ''
 
-master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
-              scorer=feedback_1040, python_lab_num='1.040', python_rubric_suffix=' - Python 1.040 Magic Genie - Rubric')
-
+if not person:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=feedback_1040, python_lab_num='1.040', python_rubric_suffix=' - Python 1.040 Magic Genie - Rubric')
+else:
+     master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                   scorer=feedback_1040, python_lab_num='1.040', python_rubric_suffix=' - Python 1.040 Magic Genie - Rubric',
+                   person=person)
