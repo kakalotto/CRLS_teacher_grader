fb9596030d469716c3a7fd91c38f0c48c9e3e467
Eric Wu <ewu@cpsd.us>
2019-09-30 11:43:54 -0400

    updated for minor bug fixes

--- wrapper_python_1.060.py
+++ wrapper_python_1.060.py
@@ -1,6 +1,12 @@
 # Pass these in as parameters
+import sys
 from master_grader import master_grader
 from CRLS_APCSP_autograder.app.python_1060 import feedback_1060
+
+
+person = ''
+if len(sys.argv) > 1:
+    person = sys.argv[1]
 
 fulltext_search = '.py'
 
@@ -18,6 +24,11 @@ def doc_name_to_rubric_name(doc_name):
 value_cells = ['F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'B9', 'B4', ]
 rubric_sheet_name = ''
 
-master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
-              scorer=feedback_1060, python_lab_num='1.060', python_rubric_suffix=' - Python 1.060 Mad Libs - Rubric')
+if not person:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=feedback_1060, python_lab_num='1.060', python_rubric_suffix=' - Python 1.060 Mad Libs - Rubric')
+else:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=feedback_1060, python_lab_num='1.060',
+                  python_rubric_suffix=' - Python 1.060 Mad Libs - Rubric', person=person)
 
