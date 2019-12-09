574ac3d159ecbc41e744fa538b2373584c6a0a09
Eric Wu <ewu@cpsd.us>
2019-12-09 09:50:18 -0500

    updated

---
+++ wrapper_python_4.037.py
@@ -0,0 +1,45 @@
+# Pass these in as parameters
+import sys
+from master_grader import master_grader
+from CRLS_APCSP_autograder.app.python_4037 import feedback_4037
+
+person = ''
+if len(sys.argv) > 1:
+    person = sys.argv[1]
+
+
+def doc_name_to_rubric_name(doc_name):
+    import re
+    p_rubric_name = doc_name
+    p_rubric_name = re.sub(r'lab',
+                           r'rubric',
+                           p_rubric_name)
+    return p_rubric_name
+
+
+
+fulltext_search = '.py'
+value_cells = [ 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'B4', 'B9', ]
+rubric_sheet_name = ''
+
+if not person:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=feedback_4037, python_lab_num='4.037',
+                  python_rubric_suffix=' - Python_4.037_Food_Trucks_rubric')
+else:
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=feedback_4037, python_lab_num='4.037',
+                  python_rubric_suffix=' - Python_4.037_Food_Trucks_rubric', person=person)
+
+
+#value_cells = ['F4', ]
+#rubric_sheet_name = ''
+
+#if not person:
+#    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+#                  scorer=docs_feedback_python_2051)
+#else:
+#    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+#                  scorer=docs_feedback_python_2051, person=person)#
+#
+#print("oooo" )
