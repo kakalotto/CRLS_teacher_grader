01cdea807cc363c5cc824e48bd0398764abf8717
Eric Wu <ewu@cpsd.us>
2019-12-16 11:45:46 -0500

    updated

--- CRLS_APCSP_autograder/app/databases_3_002.py
+++ CRLS_APCSP_autograder/app/databases_3_002.py
@@ -3,28 +3,27 @@ def docs_feedback_databases_3002(link):
 
     tests = list()
     text = get_text(link)
-
+    print(text)
     test3a = exact_answer('3a. Christmas island',
-                          [r'3a\. .+? tabledata \s* update .+? country \s set \s `*name`* \s* = '
+                          [r'3a\. .+? tabledata \s* update .+? country .+? set .+? name .*?  = '
                            r'\s* .+? holiday \s island .+? '
-                           r'where \s `*name`* \s* = \s* .+? christmas \s island .+? 3b\.',
-                           r'3a\. .+? tabledata \s* update .+? country \s set \s `*name`* \s* = '
-                           r'\s* .+? holiday \s island .+? '
-                           r'where \s `*code`* \s* = \s* .+? cxr .+? 3b\.'
+                           r'where .+? name .*? = .*? christmas \s island .+? 3b\.',
+                           r'3a\. .+? tabledata \s* update .+? country .+? set .+? name .*? \s* = \s* .*? holiday \s island .+? where .+? code .*?  = .*? cxr .+? 3b\.',
                            ], text, points=10)
     test3b = exact_answer('3b. population > 5M', [r'3b\. .+? tabledata \s* select \s* .+? \s* from \s* `*world`* \.'
                                                   r'`*country`* \s* where `* \s* `* population`* \s* > \s* 5,*000,*000 '
                                                   r'\s*'
-                                                  r'.+? 3c\.'],
+                                                  r'.+? 3c\.',
+                                                  r'3b\. .+? tabledata \s* select \s* .+? \s* from \s* `*country`* \s* where \s* `*population`* \s* > \s* 5,*000,*000 \s* .+? 3c\.  '],
                           text, points=10)
     test3c = exact_answer('3c. Beginning w/York', [r'3c\. .+? tabledata \s* select \s* .+? \s* from .+? `* city `* \s* '
-                                                   r'where \s* `*name`* \s* like .+? %york .+? 3d\.'], text, points=5)
-    test3d = exact_answer('3d. Ending w/York',  [r'3d\. .+? tabledata \s* select \s* .+? \s* from .+? `* city `* \s* '
-                                                 r'where \s* `*name`* \s* like .+? york% .+? 3e\.'], text, points=5)
-    test3e = exact_answer('3e. Avg. Africa population', [r'3e\. .+? tabledata \s* select  \s* avg  \(population\) \s* '
-                                                         r'from \s `*country`* \s* where \s* continent .+? asia .+? '
+                                                   r'where \s* `*name`* \s* like .+? york% .+? 3d\.',], text, points=5)
+    test3d = exact_answer('3d. Ending w/York',    [r'3d\. .+? tabledata \s* select \s* .+? \s* from .+? `* city `* \s* '
+                                                   r'where \s* `*name`* \s* like .+? %york .+? 3e\.'], text, points=5)
+    test3e = exact_answer('3e. Avg. Asia population', [r'3e\. .+? tabledata \s* select  \s* avg  \(population\) \s* '
+                                                         r'from \s* `*country`* \s* where .+? continent .+? asia .+? '
                                                          r'3f\.'], text, points=10)
-    test3f = exact_answer('3f. unville', [r'3f\. .+? tabledata \s*  insert \s* into \s* country .+? unville .+? $'],
+    test3f = exact_answer('3f. unville', [r'3f\. .+? tabledata \s*  insert \s* into .+? city .+? unville .+? $'],
                           text, points=10)
 
     tests.extend([test3a, test3b, test3c, test3d, test3e, test3f])
