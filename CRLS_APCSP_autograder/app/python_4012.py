da2096ab1d66d7bc1f0860bbc04954626c6e271d
eric Wu <ewu@cpsd.us>
2020-03-05 14:05:34 +0000

    updatedt o grade whther or not they pass initial tests

--- CRLS_APCSP_autograder/app/python_4012.py
+++ CRLS_APCSP_autograder/app/python_4012.py
@@ -19,45 +19,39 @@ def feedback_4012(filename):
         test_find_function = find_function(filename, 'samuel_l_algorithm', 1, points=5)
         tests.append(test_find_function)
 
-        if test_find_function['pass'] is False:
-            return tests
-        else:
-            extract_all_functions(filename)
-            function_data = extract_single_function(filename, 'samuel_l_algorithm')
-            create_testing_file(filename)
-
-            # Check for a loop of some sort (for or while)
-            test_loop = find_loop(function_data, 5)
-            test_loop['name'] += "Testing there is a loop in the samuel_l_algorithm function.<br>"
-            tests.append(test_loop)
-
-            if test_loop['pass'] is False:
-                return tests
-            else:
-                # Check that function is called 3x
-                test_function_run = function_called(filename, 'samuel_l_algorithm', 3, points=5)
-                tests.append(test_function_run)
-
-                test_ifs = find_if(function_data, 3, 5, minmax='max')
-                tests.append(test_ifs)
-
-                test_function_1 = run_unit_test('4.012', 1, 10)
-                test_function_1['name'] += " (samuel_l_algorithm with 'Birdman or (The Unexpected Virtue of Ignorance)'" \
-                                           " returns 'bad) "
-                tests.append(test_function_1)
-
-                test_function_2 = run_unit_test('4.012', 2, 10)
-                test_function_2['name'] += " (samuel_l_algorithm with 'Snakes on a plane' returns 'good') "
-                tests.append(test_function_2)
-                test_function_3 = run_unit_test('4.012', 3, 10)
-                test_function_3['name'] += " (samuel_l_algorithm with 'aladdin' returns 'maybe') "
-                tests.append(test_function_3)
-
-                # Find number of PEP8 errors and helps
-                test_pep8 = pep8(filename, 14)
-                tests.append(test_pep8)
-                test_help = helps(filename, 5)
-                tests.append(test_help)
-
-                return tests
+        extract_all_functions(filename)
+        function_data = extract_single_function(filename, 'samuel_l_algorithm')
+        create_testing_file(filename)
+        
+        # Check for a loop of some sort (for or while)
+        test_loop = find_loop(function_data, 5)
+        test_loop['name'] += "Testing there is a loop in the samuel_l_algorithm function.<br>"
+        tests.append(test_loop)
+        
+        # Check that function is called 3x
+        test_function_run = function_called(filename, 'samuel_l_algorithm', 3, points=5)
+        tests.append(test_function_run)
+        
+        test_ifs = find_if(function_data, 3, 5, minmax='max')
+        tests.append(test_ifs)
+        
+        test_function_1 = run_unit_test('4.012', 1, 10)
+        test_function_1['name'] += " (samuel_l_algorithm with 'Birdman or (The Unexpected Virtue of Ignorance)'" \
+                                   " returns 'bad) "
+        tests.append(test_function_1)
+        
+        test_function_2 = run_unit_test('4.012', 2, 10)
+        test_function_2['name'] += " (samuel_l_algorithm with 'Snakes on a plane' returns 'good') "
+        tests.append(test_function_2)
+        test_function_3 = run_unit_test('4.012', 3, 10)
+        test_function_3['name'] += " (samuel_l_algorithm with 'aladdin' returns 'maybe') "
+        tests.append(test_function_3)
+        
+        # Find number of PEP8 errors and helps
+        test_pep8 = pep8(filename, 14)
+        tests.append(test_pep8)
+        test_help = helps(filename, 5)
+        tests.append(test_help)
+        
+        return tests
             
