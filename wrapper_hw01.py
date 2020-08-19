be841b71e9d44d9e1b1c02dcc736f5d4a9341e2b
eric Wu <ewu@cpsd.us>
2020-08-19 03:57:53 +0000

    updated for hw01

--- wrapper_hw01.py
+++ wrapper_hw01.py
@@ -1,29 +1,27 @@
 # Pass these in as parameters
 import sys
 from master_grader import master_grader
-from CRLS_APCSP_autograder.app.hex_minilab import docs_feedback_hex_minilab
+from CRLS_APCSP_autograder.app.hw01 import route_docs_hw01
 
-fulltext_search = '- Hex_mini_lab'
+fulltext_search = 'HW 01, Printing'
 person = ''
 if len(sys.argv) > 1:
     person = sys.argv[1]
 
-    # NEED TO FIX HIS S2 2019/2020 2n entry b19 is a dummy.
-
-
 def doc_name_to_rubric_name(doc_name):
     import re
     p_rubric_name = doc_name
-    p_rubric_name = re.sub(r'mini_lab', r'minilab - Rubric', p_rubric_name)
+    p_rubric_name = re.sub(r'_lab', r'_rubric', p_rubric_name)
     return p_rubric_name
 
 
-value_cells = ['B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', ]
+value_cells = ['B3', 'B4', 'B5',]
 rubric_sheet_name = 'Sheet1'
+match_cells = ['D3', 'D4',]
 
 if not person:
     master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
-                  scorer=docs_feedback_hex_minilab)
+                  scorer=route_docs_hw01, match_cells=match_cells)
 else:
      master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
-                   scorer=docs_feedback_hex_minilab, person=person)
+                   scorer=route_docs_hw01, person=person, match_cells=match_cells)
