729db0ae85ec51e974dc27a7a6c8b8d472c46ae6
ewu <ewu@cpsd.us>
2020-08-19 01:14:29 -0400

    updated for HW02

--- wrapper_hw02.py
+++ wrapper_hw02.py
@@ -1,12 +1,13 @@
 # Pass these in as parameters
 import sys
 from master_grader import master_grader
-from CRLS_APCSP_autograder.app.hw01 import route_docs_hw01
+from CRLS_APCSP_autograder.app.hw02 import route_docs_hw02
 
-fulltext_search = 'HW 01, Printing'
+fulltext_search = 'HW 2, Types'
 person = ''
 if len(sys.argv) > 1:
     person = sys.argv[1]
+
 
 def doc_name_to_rubric_name(doc_name):
     import re
@@ -15,13 +16,13 @@ def doc_name_to_rubric_name(doc_name):
     return p_rubric_name
 
 
-value_cells = ['B3', 'B4', 'B5',]
+value_cells = ['B3', 'B4', 'B5']
 rubric_sheet_name = 'Sheet1'
-match_cells = ['D3', 'D4',]
+match_cells = ['D3', 'D4', 'D5']
 
 if not person:
     master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
-                  scorer=route_docs_hw01, match_cells=match_cells)
+                  scorer=route_docs_hw02, match_cells=match_cells)
 else:
-     master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
-                   scorer=route_docs_hw01, person=person, match_cells=match_cells)
+    master_grader(fulltext_search, doc_name_to_rubric_name, value_cells, sheet_name=rubric_sheet_name,
+                  scorer=route_docs_hw02, person=person, match_cells=match_cells)
